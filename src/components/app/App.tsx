d354fb8cc3825b8fb110ca499f72b39961b424aa
Vafin Artem <a.vafin85@gmail.com>
2022-05-10 18:47:24 +0500

    Добавлят browser-history

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import {Route, Routes} from 'react-router-dom';
 import './App.css';
+import browserHistory from "../../browser-history";
+import HistoryRouter from "../history-router/history-router";
 
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <HistoryRouter history={browserHistory}>
+      <Routes>
+        <Route
+          index element={
+            <div>123</div>
+          // <MainPage
+          //   selectedPointId={activeCardState.id}
+          //   onCardItemHover={onCardItemHover}
+          // />
+        }
+        />
+      </Routes>
+    </HistoryRouter>
+  )
 }
 
 export default App;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,6 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import {rootReducer} from "./root-reduser";
 
 export const store = configureStore({
   reducer: rootReducer,
 });
-
-// export type AppDispatch = typeof index.dispatch;
-// export type RootState = ReturnType<typeof index.getState>;
-// export type AppThunk<ReturnType = void> = ThunkAction<
-//   ReturnType,
-//   RootState,
-//   unknown,
-//   Action<string>
-// >;
